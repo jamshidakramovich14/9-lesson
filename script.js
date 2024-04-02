34911a4b0295c87b4124aaf5cb4ac5500989247d
jamshidakramovich14 <jamshidturdiyev761@gmail.com>
2024-04-02 14:04:53 +0500

    lesson9

---
+++ script.js
@@ -0,0 +1,203 @@
+"use strict"
+
+
+
+
+////////////////// 1-masala start //////////////////
+
+
+
+// function fasl(month){
+//     let fasl;
+//     switch(month){
+//         case 1 :
+//             console.log('Qish');
+//                   break;
+            
+//         case 2 :
+//             console.log('Qish');  
+//                  break;
+  
+
+//         case 3 :
+//             console.log('Baxor'); 
+//                  break;
+
+                
+//         case 4 :
+//             console.log('Baxor'); 
+//                  break;
+
+
+//         case 5 :
+//             console.log('Baxor'); 
+//                  break;
+
+
+//         case 6 :
+//             console.log('Yoz'); 
+//                  break;
+
+
+//         case 7 :
+//             console.log('Yoz'); 
+//                  break;
+
+
+//          case 8 :
+//             console.log('Yoz'); 
+//                  break;
+
+//         case 9 :
+//             console.log('Kuz'); 
+//                  break;
+
+//         case 10 :
+//             console.log('Kuz'); 
+//                  break;
+
+//         case 11 :
+//             console.log('Kuz'); 
+//                  break;
+
+//         case 12 :
+//             console.log('Qish'); 
+//                   break;
+
+
+//         default:
+//             fasl = ('notogri oy')
+
+//     }
+
+//     return fasl;
+// }
+
+
+// let month =prompt ('oy raqamini kiriting')*1;
+
+// let weather = fasl(month)
+// console.log(weather);
+
+
+
+////////////////// 1-masala end //////////////////
+
+
+
+
+
+////////////////// 2-masala start //////////////////
+
+
+// function age() {
+//     let yil = +prompt("tug'ulgan yilingizni kiriting");
+//     let yosh = 2024 - yil;
+//     alert( `siz ${yosh} yoshdasiz`);
+// }
+
+// age()
+
+////////////////// 2-masala end //////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////// 3-masala start //////////////////
+
+
+
+// function juftToqYigindi(n) {
+//     let juftYigindi = 0;
+//     let toqYigindi = 0;
+
+//     for (let i = 1; i <= n; i++) {
+//         if (i % 2 === 0) {
+//             juftYigindi += i;
+//         }
+//         else {
+//             toqYigindi += i;
+//         }   
+//     }
+//     let nisbat = juftYigindi / toqYigindi;
+//     return nisbat;
+// }
+
+//     let n = 10;
+//     let juftToqNisbat = juftToqYigindi(n);
+//     console.log('Juft va toq sonlar yigindisi nisbatlari: $(juftToqNisbat)');
+
+
+////////////////// 3-masala end //////////////////
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+////////////////// 4-masala start //////////////////
+
+
+// function tel() {
+//     let telCode =+prompt('telefon raqam kodingizni kiriting')
+//     switch (telCode) {
+//         case 93:
+//         case 94:
+//             alert("Ucell")
+//                 break;
+//         case 91:
+//         case 90: 
+//             alert("Beeline")
+//                 break;    
+//         case 97:
+//         case 88:
+//             alert("MobiUz")
+//                 break;
+//         case 99:
+//         case 98:
+//             alert("UzMobile")
+//              break;
+//         case 20:
+//             alert("OQ")
+//                 break;
+//         case 33:
+//             alert("Humans")
+//                 break;
+//         case 71:
+//             alert("damashni")
+//                 break;
+//         default:
+//             alert('bunaqa kompaniya mavjud emas')
+//             break;
+//     }
+// }
+
+// tel()
+
+
+
+
+////////////////// 4-masala end //////////////////
+
+
+
